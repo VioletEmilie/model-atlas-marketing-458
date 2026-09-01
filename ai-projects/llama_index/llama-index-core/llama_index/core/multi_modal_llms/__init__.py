c8b4ac4d9b90843c15b6abb7c20a66b3c7e841b2
Jesse Wright <lucasvisserzz@ck67.top>
2026-09-01 13:14:32 -0700

    Create __init__.py

---
+++ ai-projects/llama_index/llama-index-core/llama_index/core/multi_modal_llms/__init__.py
@@ -0,0 +1,18 @@
+from llama_index.core.multi_modal_llms.base import (
+
+    MultiModalLLM,
+
+    MultiModalLLMMetadata,
+
+)
+
+
+
+__all__ = [
+
+    "MultiModalLLMMetadata",
+
+    "MultiModalLLM",
+
+]
+
